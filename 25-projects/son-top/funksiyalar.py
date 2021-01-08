b2483a631c9c4f885e3bf837fdd65b5e35914ea3
Anvar <anvarbek@gmail.com>
2021-01-08 22:51:41 +0800

    25-dars qo'shildi

---
+++ 25-projects/son-top/funksiyalar.py
@@ -0,0 +1,96 @@
+"""
+08/01/2021
+
+Dasturlash asoslari
+
+"SONNI TOPISH" O'YINI
+
+Muallif: Anvar Narzullaev
+
+Web sahifa: https://python.sariq.dev
+"""
+import random
+
+def sontop(x=10):
+    tasodifiy_son = random.randint(1,x)
+    print(f"Men 1 dan {x} gacha son o'yladim. Topa olasizmi?", end="")
+    taxminlar = 0
+    while True:
+        taxminlar += 1
+        taxmin = int(input(">>>"))
+        if taxmin<tasodifiy_son:
+            print("Xato. Men o'ylagan son bundan kattaroq. Yana harakat qiling:", end="")
+        elif taxmin>tasodifiy_son:
+            print("Xato. Men o'ylagan son bundan kichikroq. Yana harakat qiling:", end="")
+        else:
+            break
+        
+    print(f"Tabriklayman. {taxminlar} ta taxmin bilan topdingiz!")
+    return taxminlar
+    
+
+def sontop_pc(x=10):
+    input(f"1 dan {x} gacha son o'ylang va istalgan tugmani bosing. Men topaman.")
+    quyi = 1
+    yuqori = x
+    taxminlar = 0
+    while True:
+        taxminlar += 1
+        if quyi != yuqori:
+            taxmin = random.randint(quyi,yuqori)
+        else:
+            taxmin = quyi
+        javob = input(f"Siz {taxmin} sonini o'yladingiz: to'g'ri (t),"
+                      f"men o'ylagan son bundan kattaroq (+), yoki kichikroq (-)".lower())
+        if javob == "-":
+            yuqori = taxmin - 1
+        elif javob == "+":
+            quyi = taxmin + 1
+        else:
+            break
+    print(f"Men {taxminlar} ta taxmin bilan topdim!")
+    return taxminlar
+
+def play(x=10):
+    yana = True
+    while yana:
+        taxminlar_user = sontop(x)
+        taxminlar_pc = sontop_pc(x)
+        
+        if taxminlar_user>taxminlar_pc:
+            print(f"Men {taxminlar_pc} taxmin bilan topdim va  yutdim!")
+        elif taxminlar_user<taxminlar_pc:
+            print(f"Siz {taxminlar_user} taxmin bilan topdingiz va yutdingiz!")
+        else:
+            print("Durrang!")
+        yana = int(input("Yana o'ynaymizmi? Ha(1)/Yo'q(0):"))
+            
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+# son_top()
---
+++ 25-projects/son-top/main.py
@@ -0,0 +1,16 @@
+"""
+08/01/2021
+
+Dasturlash asoslari
+
+"SON TOPISH" O'YINI
+
+Muallif: Anvar Narzullaev
+
+Web sahifa: https://python.sariq.dev
+"""
+
+import funksiyalar as f
+
+print("Keling o'ylagan sonni topish o'ynaymiz!")
+f.play(10)
