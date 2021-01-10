e6df0a2413193c222d3db5d75f5edb15d2845862
Anvar <anvarbek@gmail.com>
2021-01-10 23:03:47 +0800

    26-dars qo'shildi

---
+++ 26-projects/soz-topish/funksiyalar.py
@@ -0,0 +1,56 @@
+"""
+09/01/2021
+
+Dasturlash asoslari
+
+"SO'Z TOPISH" O'YINI
+
+Muallif: Anvar Narzullaev
+
+Web sahifa: https://python.sariq.dev
+"""
+
+import random
+from uzwords import words
+
+def get_word():
+    word = random.choice(words)
+    while "-" in word or ' ' in word:
+        word = random.choice(words)    
+    return word.upper()
+
+def display(user_letters,word):
+    display_letter=""
+    for letter in word:
+        if letter in user_letters:
+            display_letter += letter
+        else:
+            display_letter += "-"
+    return display_letter
+
+def play():
+    word = get_word()
+    # So'zdagi harflar
+    word_letters = set(word)
+    # Foydalanuvchi kiritgan harflar
+    user_letters = ''
+    print(f"Мен {len(word)} хонали сўз ўйладим. Топа оласизми?")
+    # print(word)
+    while word_letters:
+        print(display(user_letters,word))
+        if user_letters:
+            print(f"Шу вақтгача киритган ҳарфларингиз: {user_letters}")
+        
+        letter = input("Ҳарф киритинг: ").upper()
+        if letter in user_letters:
+            print("Бу ҳарфни аввал киритгансиз. Бошқа ҳарф киритинг.")
+            continue        
+        elif letter in word:
+            word_letters.remove(letter)
+            print(f"{letter} ҳарфи тўғри.")
+        else:
+            print("Бундай ҳарф йўқ.")
+        user_letters += letter
+    print(f"Табриклайман! {word} сўзини {len(user_letters)} та уринишда топдингиз.")
+    
+
---
+++ 26-projects/soz-topish/main.py
@@ -0,0 +1,14 @@
+"""
+09/01/2021
+
+Dasturlash asoslari
+
+"SO'Z TOPISH" O'YINI
+
+Muallif: Anvar Narzullaev
+
+Web sahifa: https://python.sariq.dev
+"""
+from funksiyalar import play
+
+play()
