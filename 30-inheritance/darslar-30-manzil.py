36404b4da218d8598197e0d320e35cdd101cd435
Anvar <anvarbek@gmail.com>
2021-01-29 15:42:13 +0800

    30-dars qo'shildi

---
+++ 30-inheritance/darslar-30-manzil.py
@@ -0,0 +1,78 @@
+"""
+28/01/2021
+
+Dasturlash asoslari
+
+#30-DARS. VORISLIK VA POLIMORFIZM
+
+Muallif: Anvar Narzullaev
+
+Web sahifa: https://python.sariq.dev
+"""
+
+class Shaxs:
+    """Shaxslar haqida ma'lumot"""
+    def __init__(self,ism,familiya,passport,tyil):
+        """Shaxsning xususiyatlari"""
+        self.ism = ism
+        self.familiya = familiya
+        self.passport = passport
+        self.tyil = tyil
+    
+    def get_info(self):
+        """Shaxs haqida ma'lumot"""
+        info = f"{self.ism} {self.familiya}. "
+        info += f"Passport:{self.passport}, {self.tyil}-yilda tug`ilgan"
+        return info
+    def get_age(self,yil):
+        """Shaxsning yoshini qaytaruvchi metod"""
+        return yil - self.tyil
+
+class Talaba(Shaxs):
+    """Talaba klassi"""
+    def __init__(self,ism,familiya,passport,tyil,idraqam,manzil):
+        """Talabaning xususiyatlari"""
+        super().__init__(ism, familiya, passport, tyil)
+        self.idraqam = idraqam
+        self.bosqich = 1
+        self.manzil = manzil
+    
+    def get_id(self):
+        """Talabaning ID raqami"""
+        return self.idraqam
+    
+    def get_bosqich(self):
+        """Talabaning o'qish bosqichi"""
+        return self.bosqich
+    
+    def get_info(self):
+        """Talaba haqida ma'lumot"""
+        info = f"{self.ism} {self.familiya}. "
+        info += f"{self.get_bosqich()}-bosqich. ID raqami: {self.idraqam}"
+        return info
+
+    
+class Manzil:
+    """Manzil saqlash uchun klass"""
+    def __init__(self,uy,kocha,tuman,viloyat):
+        """Manzil xususiyatlari"""
+        self.uy = uy
+        self.kocha = kocha
+        self.tuman = tuman
+        self.viloyat = viloyat
+    
+    def get_manzil(self):
+        """Manzilni ko'rish"""
+        manzil = f"{self.viloyat} viloyati, {self.tuman} tumani, "
+        manzil += f"{self.kocha} ko'chasi, {self.uy}-uy"
+        return manzil
+
+talaba1_manzil = Manzil(12,"Olmazor","Bog'bon","Samarqand")
+talaba1 = Talaba("Valijon","Aliyev","FA112299",2000,"0000012",talaba1_manzil)
+
+
+       
+
+
+
+
---
+++ 30-inheritance/darslar-30-shaxs.py
@@ -0,0 +1,51 @@
+"""
+28/01/2021
+
+Dasturlash asoslari
+
+#30-DARS. VORISLIK VA POLIMORFIZM
+
+Muallif: Anvar Narzullaev
+
+Web sahifa: https://python.sariq.dev
+"""
+
+class Shaxs:
+    """Shaxslar haqida ma'lumot"""
+    def __init__(self,ism,familiya,passport,tyil):
+        """Shaxsning xususiyatlari"""
+        self.ism = ism
+        self.familiya = familiya
+        self.passport = passport
+        self.tyil = tyil
+    
+    def get_info(self):
+        """Shaxs haqida ma'lumot"""
+        info = f"{self.ism} {self.familiya}. "
+        info += f"Passport:{self.passport}, {self.tyil}-yilda tug`ilgan"
+        return info
+    def get_age(self,yil):
+        """Shaxsning yoshini qaytaruvchi metod"""
+        return yil - self.tyil
+
+class Talaba(Shaxs):
+    """Talaba klassi"""
+    def __init__(self,ism,familiya,passport,tyil,idraqam):
+        """Talabaning xususiyatlari"""
+        super().__init__(ism, familiya, passport, tyil)
+        self.idraqam = idraqam
+        self.bosqich = 1
+    
+    def get_id(self):
+        """Talabaning ID raqami"""
+        return self.idraqam
+    
+    def get_bosqich(self):
+        """Talabaning o'qish bosqichi"""
+        return self.bosqich
+    
+    def get_info(self):
+        """Talaba haqida ma'lumot"""
+        info = f"{self.ism} {self.familiya}. "
+        info += f"{self.get_bosqich()}-bosqich. ID raqami: {self.idraqam}"
+        return info
